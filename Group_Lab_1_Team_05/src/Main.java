6ef8b6504b08b63011df6d794758ca56be898e0d
Hannah Chiou <chiou.h@northeastern.edu>
2026-02-20 16:07:21 -0500

    Adding member 2 files

--- Group_Lab_1_Team_05/src/Main.java
+++ Group_Lab_1_Team_05/src/Main.java
@@ -12,11 +12,11 @@ public class Main {
     public static void main(String[] args) {     
         //Member1.main(args);
 
-        //Member2.main(args);
+        Member2.main(args);
 
         //Member3.main(args);
        
-        Member4.main(args); 
+        // Member4.main(args); 
         
 
     }
---
+++ Group_Lab_1_Team_05/src/Member2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hannahchiou
+ */
+class Member2 {
+
+    static void main(String[] args) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
